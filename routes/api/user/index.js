cc87f3f7307a25e585dddf18ed331b257ea90a0d
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-03-28 01:10:17 +0900

    asdf

--- routes/api/user/index.js
+++ routes/api/user/index.js
@@ -3,5 +3,9 @@ const controller = require('./user.controller');
 
 // router.post('/user', controller.user);
 router.get('/me', controller.getMyAccountInfo);
+router.get('/myposts', controller.getMyPosts);
+router.get('/myreviews', conroller.getMyReviews);
+router.get('/mylikes', controller.getMyLikes);
+
 
 module.exports = router;
--- routes/api/user/user.controller.js
+++ routes/api/user/user.controller.js
@@ -15,3 +15,42 @@ exports.getMyAccountInfo = (req, res) => {
     }
   )
 }
+
+exports.getMyPosts = (req, res) => {
+  conn.query(
+    'SELECT count(*) from Posts where user_id = ?',
+    [req.decoded._id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({
+        result
+      })
+    }
+  )
+}
+
+exports.getMyLikes = (req, res) => {
+  conn.query(
+    'SELECT sum(like_cnt) from Posts where user_id = ?',
+    [req.decoded._id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({
+        result
+      })
+    }
+  )
+}
+
+exports.getMyReviews = (req, res) => {
+  conn.query(
+    'SELECT count(*) from Comments where user_id = ?',
+    [req.decoded._id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({
+        result
+      })
+    }
+  )
+}
