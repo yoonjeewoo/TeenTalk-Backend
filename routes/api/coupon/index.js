8e7ee2c4ebf9806b9b357c8a8ee31106009e235e
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-03-29 00:32:55 +0900

    asdf

--- routes/api/coupon/index.js
+++ routes/api/coupon/index.js
@@ -7,6 +7,6 @@ router.get('/image/:coupon_id', controller.getCouponImageList);
 router.get('/review/:coupon_id', controller.getCouponReviewList);
 
 router.post('/review/:coupon_id', controller.createCouponReview);
-router.delte('/review/:coupon_id', controller.deleteCouponReview);
+router.delete('/review/:coupon_id', controller.deleteCouponReview);
 
 module.exports = router;
