7861dd36f5a22760f9c9de0c90cfdd585326b52a
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-05-03 20:39:48 +0900

    created_at 추가

--- routes/api/tint/tint.controller.js
+++ routes/api/tint/tint.controller.js
@@ -4,10 +4,12 @@ const config = require('../../../config');
 const conn = mysql.createConnection(config);
 
 exports.uploadTint = (req, res) => {
+    const d = new Date();
+    d.setUTCHours(d.getUTCHours());
 	const { title, content, video_id, category } = req.body;
 	conn.query(
-		'INSERT INTO Tints(title, content, video_id, user_id, like_cnt, category) VALUES(?, ?, ?, ? ,? ,?)',
-		[title, content, video_id, req.decoded._id, 0, category],
+		'INSERT INTO Tints(title, content, video_id, user_id, like_cnt, category, created_at) VALUES(?, ?, ?, ? ,? ,?, ?)',
+		[title, content, video_id, req.decoded._id, 0, category, d],
 		(err, result) => {
 			if (err) throw err;
 			return res.status(200).json({
@@ -20,7 +22,7 @@ exports.uploadTint = (req, res) => {
 exports.getTintList = (req, res) => {
 	if (req.query.category == 0) {
 		conn.query(
-			'SELECT Tints.title, Tints.content, Tints.video_id, Users.id, Users.username, Tints.like_cnt FROM Tints JOIN Users on Tints.user_id = Users.id',
+			'SELECT Tints.title, Tints.content, Tints.video_id, Users.id, Users.username, Tints.like_cnt, Tints.created_at FROM Tints JOIN Users on Tints.user_id = Users.id',
 			(err, result) => {
 				if (err) throw err;
 				return res.status(200).json({
