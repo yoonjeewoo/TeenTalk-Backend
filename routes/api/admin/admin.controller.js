7c8e05668cdaed1b047c55f6286acb2d66c77d28
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-05-04 17:02:00 +0900

    asdf

--- routes/api/admin/admin.controller.js
+++ routes/api/admin/admin.controller.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const mysql = require('mysql');
 const config = require('../../../config');
 const conn = mysql.createConnection(config);
+const crypto = require("crypto");
 
 exports.homeSlideImage = (req, res) => {
   const { pic_list } = req.body;
