fdea3b695375bbce24cb119b92b6e68c3e055f1d
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-05-04 16:17:22 +0900

    asdf

--- routes/api/tint/index.js
+++ routes/api/tint/index.js
@@ -5,8 +5,8 @@ router.post('', controller.uploadTint);
 router.get('/list', controller.getTintList);
 router.get('/image', controller.getTintImage);
 // router.get('')
-router.post('/like', controller.likeTint);
-router.get('/like', controller.likeCheck);
-router.post('/unlike', controller.unlikeTint);
+router.post('/like/:tint_id', controller.likeTint);
+router.get('/like/:tint_id', controller.likeCheck);
+router.post('/unlike/:tint_id', controller.unlikeTint);
 
 module.exports = router;
