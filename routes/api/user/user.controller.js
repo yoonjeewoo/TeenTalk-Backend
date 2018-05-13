feb86ea2128efa43f6b0a0c8cde3e68c2be22f2f
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-05-13 21:19:17 +0900

    final

--- routes/api/user/user.controller.js
+++ routes/api/user/user.controller.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const mysql = require('mysql');
 const config = require('../../../config');
 const conn = mysql.createConnection(config);
+const crypto = require('crypto');
 
 exports.getMyAccountInfo = (req, res) => {
   conn.query(
@@ -68,3 +69,33 @@ exports.getMyCommentCount = (req, res) => {
   )
 };
 
+exports.updateMyInfo = (req, res) => {
+  const { username, password, birth } = req.body;
+  const encrypted = crypto.createHmac('sha1', config.secret)
+    .update(password)
+    .digest('base64');
+  conn.query(
+    "UPDATE Users SET username = ?, password = ?, birth = ? WHERE id = ?",
+    [username, encrypted, birth, req.decoded._id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({
+        message: 'successfully updated private information'
+      })
+    }
+  )
+}
+
+exports.updateMySchoolInfo = (req, res) => {
+  const { school_id, class_, class_num } = req.body;
+  conn.query(
+    "UPDATE Users SET school_id = ?, class = ?, class_num = ? WHERE id = ?",
+    [school_id, class_, class_num, req.decoded._id],
+    (err, result) => {
+      if (err) throw err;
+      return res.status(200).json({
+        message: 'successfully updated school information'
+      })
+    }
+  )
+}
